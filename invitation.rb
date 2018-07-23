0f6e4b6b61ce37e993ac46c48c99ee02b82c5bc1
aleajane <heyya.its.alea@gmail.com>
2018-07-23 19:45:00 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -1,4 +1,4 @@
 # Code your prompts here!
 
 # Try starting out with puts'ing a string.
-"Hi, you've been invited to a party! What is your name?"), "Make sure something is printed to the console that outputs 'Hi, you've been invited to a party! What is your name?' exactly."
+puts "Hi, you've been invited to a party! What is your name?"
